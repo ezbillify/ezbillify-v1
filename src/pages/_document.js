ca98e8ea5c934428c5e90aee45fd6cf3dbb932f4
Ezbillify <ezbillify@gmail.com>
2025-09-22 15:05:39 +0530

    favicon update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,7 +12,7 @@ export default function Document() {
         <meta name="author" content="EzBillify" />
         
         {/* Favicon */}
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/ezbillifyfavicon.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
